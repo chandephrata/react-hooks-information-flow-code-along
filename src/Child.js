f8d47f87f49f8b9160d3fc79c2bfd6a4694d3e48
Chandelier Ephrata <chandephrata@gmail.com>
2022-04-07 19:47:57 -0600

    commit tree 3df6070f5233b71e52a10fcc8f054541e3684f07

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+
+//Added event handler onClick inside of the <div>
+function Child({ onChangeColor, color }) {    //initial function with "onChangeColor as a prop"
+  //console.log(onChangeColor)
+  function handleClick(){
+    const newColor = getRandomColor()
+    onChangeColor(newColor)
+  }
+  return(
+   <div onClick={handleClick} className="child" style={{ backgroundColor: color }} /> 
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
-function Parent() {
+function Parent() {   // initial function
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor);  //initial value for color state
+  const [childrenColor, setChildrenColor] = useState('#FFF')
 
+function handleChangeColor(newChildColor){     // created a new function to update state, and added newChildColor as an argument
+  const newRandomColor = getRandomColor();
+  setColor(newRandomColor)      //update color state to a new value
+  setChildrenColor(newChildColor)
+}
+  // On both <Child/> we are going to pass this state chaning function as a prop to both child component
+  //both <Child/> were updated/added under the initail <div/>
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>    
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
